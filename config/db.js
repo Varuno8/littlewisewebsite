f77eaa86f70f4d17ef5c0cafbbb3db82b9e4d387
NIKHIL KANOJIA <nikhilkanojia693@gmail.com>
2025-02-25 00:46:26 +0530

    fix some errors

--- config/db.js
+++ config/db.js
@@ -16,7 +16,7 @@ async function connectDB() {
             bufferCommands: false,
         }
 
-        cached.promise = mongoose.connect('${ process.env.MONGODB_URI } / littlewise', opts).then(mongoose => {
+        cached.promise = mongoose.connect(`${ process.env.MONGODB_URI } / littlewise`, opts).then(mongoose => {
             return mongoose
         })
     } 
