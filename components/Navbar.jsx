ce26687b1235c7ba8e6eb43d6a862a4d48566200
Varun <varunx723@gmail.com>
2025-02-27 00:51:17 +0530

    `Update Navbar component: change "Home" to "HOME" in two links`

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
       />
       <div className="flex items-center gap-4 lg:gap-8 max-md:hidden">
         <Link href="/" className="hover:text-gray-900 transition">
-          Home
+          HOME
         </Link>
         <Link href="/all-products" className="hover:text-gray-900 transition">
           Shop
