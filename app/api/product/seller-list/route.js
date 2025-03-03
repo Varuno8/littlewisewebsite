c8826f28fbbf512daf45be3f846cdc1fa564f148
Varun <varunx723@gmail.com>
2025-03-04 01:46:45 +0530

    Update import path for @clerk/nextjs/server

--- app/api/product/seller-list/route.js
+++ app/api/product/seller-list/route.js
@@ -1,5 +1,5 @@
 import authSeller from "@/lib/authSeller";
-import { getAuth } from "@clerk/nextjs/dist/types/server";
+import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function Get(request) {
