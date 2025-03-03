0ce196df428cfba72ffb0661b802b898809f639a
Varun <varunx723@gmail.com>
2025-03-03 23:43:42 +0530

    feat: added authentication token and form data handling in AddProduct component

--- app/seller/page.jsx
+++ app/seller/page.jsx
@@ -2,8 +2,11 @@
 import React, { useState } from "react";
 import { assets } from "@/assets/assets";
 import Image from "next/image";
+import { useAppContext } from "@/context/AppContext";
 
 const AddProduct = () => {
+
+  const{getToke}=useAppContext()
 
   const [files, setFiles] = useState([]);
   const [name, setName] = useState('');
@@ -15,7 +18,36 @@ const AddProduct = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-  };
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('description', description);
+    formData.append('category', category);
+    formData.append('price', price);
+    formData.append('offerprice', offerPrice);
+
+    for(let i = 0; i < files.length; i++){
+      formData.append('images', files[i]);
+    };
+
+    try {
+      const token = await getToken();
+      const { data } = await axios.post('/api/product/add', formData, { headers: { Authorization: `Bearer ${token}` } });
+      if (data.success) {
+        toast.success(data.message);
+        setFiles([]);
+        setName('');
+        setDescription('');
+        setCategory('Earphone');
+        setPrice('');
+        setOfferPrice('');
+      }else{
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(data.message);
+    }
+    
+    const {data}=await axios.post('/api/product/add',formData,{})
 
   return (
     <div className="flex-1 min-h-screen flex flex-col justify-between">
